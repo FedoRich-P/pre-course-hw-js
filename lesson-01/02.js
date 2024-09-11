af5073f491d5cec2821b8a61c445e865227da3dc
FedoRich-P <panchev_fedor@mail.ru>
2024-09-11 12:49:44 +0300

    add background-color

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,4 +1,4 @@
 // code
 let number1 = Math.floor(Math.random() * 100)
-
-console.log(number1 % 2 ? `${number1}:Это не четное число` : `${number1}:Это четное число` )
+let res = number1 % 2 ? `${number1}:Это не четное число` : `${number1}:Это четное число`
+console.log(res)
