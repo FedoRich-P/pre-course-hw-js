283a8d22a9bc7c5cba5c8e60b44c6306a886632d
FedoRich-P <panchev_fedor@mail.ru>
2024-09-26 23:33:08 +0300

    h.w.6

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,10 +1,6 @@
 let str = "zzzyzzz";
 
-const searchLetter = (word, letter) => {
-   return  word.includes(letter) ? 'нашел' : 'заблудился'
-}
-
-console.log(searchLetter(str, 'y'))
+console.log(str.includes('y') ? 'нашел':'заблудился' );
 
 
 
