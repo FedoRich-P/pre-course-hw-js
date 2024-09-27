6732799cf99222b395a99c33522d7de122f4301a
FedoRich-P <panchev_fedor@mail.ru>
2024-09-27 12:01:44 +0300

    2.7

---
+++ lesson-02/07.js
@@ -0,0 +1,18 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let passportMarried2 = {
+    ...passportMarried,
+    married: true,
+    address: {...passportMarried.address}
+}
+passportMarried2.married = false
+
+console.log(passportMarried);
+console.log(passportMarried2);
