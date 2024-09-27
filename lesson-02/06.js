ee520e344df5374813d6613a49b223dd4c96f184
FedoRich-P <panchev_fedor@mail.ru>
2024-09-27 11:20:45 +0300

    2.6

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -1,1 +1,19 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
 
+const newPassportWithAddress = {
+    ...passportWithAddress,
+    address: {
+        ...passportWithAddress.address,
+        city: 'Bobryisk'
+    }
+}
+
+console.log(passportWithAddress.address.city)
+console.log(newPassportWithAddress.address.city)
